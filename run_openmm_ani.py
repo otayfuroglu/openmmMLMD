b0db0f10dd72190b015ac926685ed28898d952c1
otayfuroglu <https://github.com/otayfuroglu/deepMOF.git>
2023-02-02 05:56:34 +0300

    error debugged

--- run_openmm_ani.py
+++ run_openmm_ani.py
@@ -1,4 +1,7 @@
 #
+import os
+import pandas as pd
+import csv
 from openmm import app
 from openmm import unit
 from openmmml import MLPotential
@@ -7,18 +10,27 @@ from openmm_funcs import (equilibrate,
 
 
 # set device
-device = "CUDA"
+device = "CPU"
+#  device = "CUDA"
+#  device = "OpenCL"
 
 # Load from gromac files
 #  gro = app.GromacsGroFile("mol.gro")
 #  forcefield = app.GromacsTopFile("mol.top",
 #                                  periodicBoxVectors=gro.getPeriodicBoxVectors())
+#file1 = open("./list.txt", 'r')
+#file2 = file1.readlines()
+#for mole in file2:
+#    print(mole)
+#    mole=mole.replace("\n","")
+#    print(mole)
 
-# Load from pdb
-pdb = app.PDBFile("minimized_struct.pdb")
+    # Load from pdb
+pdb_path = "/truba_scratch/otayfuroglu/openmmMLMD/test/mudu_works/test_1mole/mobley_5631798.pdb"
+pdb = app.PDBFile(pdb_path)
 temp = 300
 step_size = 2 * unit.femtoseconds
-total_time = 1*unit.nanoseconds,
+total_time = 1*unit.nanoseconds
 
 # set ani potential as a forcefield
 forcefield = MLPotential('ani2x')
@@ -34,10 +46,11 @@ modeller = app.Modeller(pdb.topology, pdb.positions)
 simulation = equilibrate(
     modeller,
     forcefield,
-    temp_range=range(100, temp, 50),
+    temp_range=range(100, temp+1, 100),
     time_per_temp_increment=0.001*unit.nanoseconds,
     time_final_stage=0.01*unit.nanoseconds,
     step_size=step_size,
+    minimize=False,
     platform=device,
 )
 
